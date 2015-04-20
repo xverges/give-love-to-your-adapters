9bff7ba153ea5b92d5eabdb9354b1317b2340548
Xavier <xverges@gmail.com>
2015-04-20 14:42:40 +0200

    grunt jshint ready

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,22 @@ module.exports = function(grunt) {
 
     var path = require('path');
 
+    var patterns = {
+        js: {
+            gruntfile: 'Gruntfile.js',
+            adapters: 'adapters/**/*.js',
+            apps: ['apps/*/common/js/**/*.js', '!apps/*/common/js/lib/**/*.js'],
+            libs: 'apps/*/common/js/lib/**/*.js',
+            testAdapters: 'test/js/server/**/*.js',
+            testApps: 'test/js/apps/**/*.js',
+            testLib: 'test/js/lib/*.js'
+        },
+        html: {
+            index: 'apps/*/common/index.html'
+        }
+    };
+
+
     grunt.initConfig({
         express: {
             backend: {
@@ -12,8 +28,30 @@ module.exports = function(grunt) {
                     port: 3000
                 }
             }
+        },
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            gruntfile: {
+                src: [patterns.js.gruntfile]
+            },
+            js: {
+                src: [patterns.js.adapters,
+                      patterns.js.apps,
+                      patterns.js.testAdapters,
+                      patterns.js.testApps,
+                      patterns.js.testLib]
+            },
+            html: {
+                src: [patterns.html.index],
+                options: {
+                    extract: 'always'
+                }
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-express');
 };
